6309eae344174442a2632a0f5c6f4ba94fe0aae0
broadbounds <84296908+broadbounds@users.noreply.github.com>
2021-05-18 20:25:36 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   version = "v2.70.0"
 }
 
-resource "aws_instance" "first_ec2" {
-  ami           = "ami-07a3e3eda401f8caa"
-  instance_type = "t2.micro"
+resource "aws_instance" "example" {
+   ami           = "ami-077e31c4939f6a2f3"
+   instance_type = "t2.micro"
 }
