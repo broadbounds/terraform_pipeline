1b8854cdf52a958d462126f6512c52695daae001
broadbounds <84296908+broadbounds@users.noreply.github.com>
2021-05-18 20:26:18 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 provider "aws" {
-  region = "us-east-2"
-  version = "v2.70.0"
+   region  = "us-east-2"
+   access_key = var.access_key
+   secret_key = var.secret_key
 }
 
 resource "aws_instance" "example" {
