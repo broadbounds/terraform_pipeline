13fe1858627249a755e82d4e214b4f5e44483a2b
broadbounds <84296908+broadbounds@users.noreply.github.com>
2021-05-18 23:07:53 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,3 +10,7 @@ resource "aws_instance" "bastion_host" {
       Name = "bastion host"
    }
 }
+resource "aws_instance" "example" {
+   ami           = "ami-077e31c4939f6a2f3"
+   instance_type = "t2.micro"
+}
